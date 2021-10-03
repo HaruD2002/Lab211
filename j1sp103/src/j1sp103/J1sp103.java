2024f467bc4de979e3b19d5da8c2c8b26251a744
HaruD2002 <duynhatdinh2002@gmail.com>
2021-10-03 21:42:20 +0700

    lab211_done

--- j1sp103/src/j1sp103/J1sp103.java
+++ j1sp103/src/j1sp103/J1sp103.java
@@ -12,25 +12,50 @@ import java.text.SimpleDateFormat;
  * @author Admin
  */
 public class J1sp103 {
-
+    private static final Scanner sc  = new Scanner (System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ParseException {
-    Scanner sc  = new Scanner (System.in);
-    System.out.print("Enter Date 1:");
-    String sDate1 = sc.nextLine();
-    System.out.print("Enter Date 2:");
-    String sDate2= sc.nextLine();
-    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
-    Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(sDate2);
     
-    if(sDate1.compareToIgnoreCase(sDate2) >0)
+    System.out.print("Enter Date 1:");
+    String s1 = takeInput();
+    System.out.print("Enter Date 2:");
+    String s2= takeInput();
+
+    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(s1);
+    Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(s2);
+
+    if(date1.before(date2))
             System.out.println("Date 1 is before date 2");
-    else 
+    else
                 System.out.println("Date 1 is after date 2");
-   
-        
+
+
     }
     
+    public static String takeInput(){
+        String s;
+        while(true){
+            s = sc.nextLine().trim();
+            if(s.isEmpty()) 
+                System.out.print("Empty Date. Please re-enter");
+            
+            if(checkFormat(s) == true)
+                return s; 
+            else
+                System.out.print("Not value answer.Please re-enter: ");
+        }
+    }
+
+    public static boolean checkFormat(String s){
+        String regex  = "^[0-3]{1,9}(\\/[0-9]{1})(\\/[0-9]{0,9})?$";
+        
+        if(s.matches(regex))
+            return true;
+        else 
+            return false;
+        
+    }
+
 }
